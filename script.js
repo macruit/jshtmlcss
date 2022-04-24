338074e01eb832d4d63acd5fd1dd9cead7c28ebe
macruit <mac.ru.it@gmail.com>
2022-04-24 20:20:45 +0200

    new add

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.querySelector('.html').onclick = () =>{
-    alert('HTML');
+menu.onclick = function myFunction(){
+var x= document.getElementById('myTopnav');
+if(x.className === 'topnav'){
+    x.className += " responsive";
+} else{
+    x.className = 'topnav';
+}
 }
