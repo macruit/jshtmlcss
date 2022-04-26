77c3c449d0862bb5b1f80e79995f47efbaef5319
macruit <mac.ru.it@gmail.com>
2022-04-26 02:55:19 +0200

    26.04.300

--- script.js
+++ script.js
@@ -6,3 +6,51 @@ if(x.className === 'topnav'){
     x.className = 'topnav';
 }
 }
+
+
+// Todo List
+
+let input = document.getElementById('todo-input');
+let btn = document.querySelector('.todo-button');
+let result = document.querySelector('.todo-out');
+
+//Button Input onclick
+btn.addEventListener('click', (e) =>{
+    
+    if(input === '') return
+    createDeleteElements(input.value)
+    input.value = '';
+});
+//Button Input click Ende
+
+
+function createDeleteElements(value){
+    
+
+    const btn = document.createElement('button');
+    const li = document.createElement('li');
+
+    li.className = 'li';
+    li.textContent = value;
+
+    btn.className = 'btn';
+    btn.textContent = '-';
+    li.appendChild(btn);
+
+btn.addEventListener('click', (e) =>{
+    
+    result.removeChild(li);
+})
+
+li.addEventListener('click', (e) => {
+    li.classList.toggle('li-active');
+})
+
+    result.appendChild(li);
+}
+
+
+
+
+
+// Todo lis END
